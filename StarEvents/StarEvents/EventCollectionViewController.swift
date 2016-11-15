647347d7e449629c354226322e8647306d77929c
Liam Butler-Lawrence <liamdunn@icloud.com>
2016-11-14 23:25:33 -0500

    Add empty view controllers with collection view

---
+++ StarEvents/StarEvents/EventCollectionViewController.swift
@@ -0,0 +1,15 @@
+//
+//  EventCollectionViewController.swift
+//  StarEvents
+//
+//  Created by Liam Butler-Lawrence on 11/14/16.
+//  Copyright © 2016 Liam Butler-Lawrence. All rights reserved.
+//
+
+import UIKit
+
+class EventCollectionViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+
+}
